3776c500835f3080c4dee13bd57afe0badfb579c
akmiller01 <alex.k.miller@gmail.com>
2022-07-26 13:25:07 -0400

    Test and dependabot

---
+++ tests/sample_output_test.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+class TestSampleOutput:
+    @pytest.mark.parametrize(
+        ('file_path', 'expected_snippet'),
+        [
+            ("en/_build/dirhtml/index.html", "Standard guidance"),
+        ]
+    )
+    def test_sample_output(self, file_path, expected_snippet):
+        with open(file_path, "r") as open_file:
+            file_contents = open_file.read()
+            assert expected_snippet in file_contents
